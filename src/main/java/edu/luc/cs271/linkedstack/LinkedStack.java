fe8218fb55ed4b0f21855e41e683fcda556e1c7f
Ivan Kavanagh <ikavanagh@luc.edu>
2017-10-17 03:07:13 +0000

    lab 7

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -1,44 +1,71 @@
+
 package edu.luc.cs271.linkedstack;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 
 public class LinkedStack<E> implements IStack<E> {
 
   /** The topmost node of this stack. The stack gets pushed down from here. */
   private Node<E> top;
+  private List<E> listemptyData;
+  private List<E> listnonemptyData;
 
   // TODO why don't we need an explicit constructor?
 
   @Override
   public E push(final E obj) {
-    // TODO
-    return null;
-  }
+   top = new Node<>(obj,top); 
+   return obj;
+  
+}
+    
+  
 
   @Override
   public E peek() {
     // TODO
-    return null;
+    if(isEmpty()) 
+    {
+      throw new NoSuchElementException();
+    }
+    else
+    {
+      return top.data;
+    }
   }
 
   @Override
   public E pop() {
     // TODO
-    return null;
+    if(isEmpty()) {
+    throw new NoSuchElementException();
+    }
+    else
+    {
+      E output = top.data;
+      top = top.next; 
+      return output;
+    }
   }
 
   @Override
   public boolean isEmpty() {
     // TODO
-    return false;
+    return (top == null);
   }
 
   @Override
   public List<E> asList() {
     // TODO implement using an ArrayList preallocated with the right size
     // TODO add any instance variable(s) required to support this
-    return null;
+   List<E> result = new ArrayList<E>();
+   while(isEmpty() == false){
+     result.add(pop());
+   }
+   return result;
+    
   }
 }
